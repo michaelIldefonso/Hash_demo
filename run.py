5e6e243e038bf92ef158ab935d448d4ca8daaedd
Michael Ildefonso <110015305+MCHLIL@users.noreply.github.com>
2024-10-31 17:59:43 +0800

    aa

--- run.py
+++ run.py
@@ -1,1 +1,8 @@
- 
+from app import create_app
+
+# Create an instance of the Flask app
+app = create_app()
+
+if __name__ == '__main__':
+    # Run the Flask application
+    app.run(host='0.0.0.0', port=5000, debug=True)
