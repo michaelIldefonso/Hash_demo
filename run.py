ddd65ff47a45327fd7097e9f65d7be66ede72b86
Ken Lacanienta <kenlacanienta93@gmail.com>
2024-10-31 22:32:13 +0800

    dami

--- run.py
+++ run.py
@@ -1,8 +1,33 @@
-from app import create_app
+from flask import Flask, render_template, request, redirect, url_for
+from app.models import db, User
 
-# Create an instance of the Flask app
-app = create_app()
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///example.db'  # Use SQLite for this example
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db.init_app(app)
+
+@app.route('/')
+def index():
+    users = User.query.all()
+    return render_template('index.html', users=users)
+
+@app.route('/add', methods=['POST'])
+def add_user():
+    userName = request.form.get('userName')
+    fullName = request.form.get('fullName')
+    email = request.form.get('email')
+    contact = request.form.get('contact')
+    password = request.form.get('password')
+
+    new_user = User(userName=userName, fullName=fullName, email=email,
+                    contact=contact, password=password)
+    
+
+    db.session.add(new_user)
+    db.session.commit()
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
-    # Run the Flask application
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    with app.app_context():
+        db.create_all()  # Create database tables
+    app.run(debug=True)
