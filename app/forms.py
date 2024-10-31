b8690229089feb37367e53a418ff081160c671c4
Ken Lacanienta <kenlacanienta93@gmail.com>
2024-10-31 21:17:07 +0800

    dami

--- app/forms.py
+++ app/forms.py
@@ -8,7 +8,10 @@ class LoginForm(FlaskForm):
     submit = SubmitField('Login')
 
 class SignupForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
-    password = PasswordField('Password', validators=[DataRequired()])
-    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
-    submit = SubmitField('Sign Up')
+    email = StringField('Email', validators=[DataRequired(), Length(min=2, max=50)])
+    userName = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
+    fullName = StringField('Fullname', validators=[DataRequired(), Length(min=2)])
+    contact = StringField('Contact', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=12)])
+    pass2 = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    confirmBtn = SubmitField('Sign Up')
